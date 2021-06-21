eb2508ac2923e1ce936285fb3454dd79b9fb2739
Xrustaller <stalker0077@gmail.com>
2021-06-22 01:20:50 +0300

    libs

---
+++ libraries/DTM1650/src/DTM1650.cpp
@@ -0,0 +1,128 @@
+#include <Wire.h>
+#include "DTM1650.h"
+
+DTM1650::DTM1650(void) {
+	localI2CDisplayAddress = DTM1650_DISPLAY_BASE;
+	localI2CControlAddress = DTM1650_CONTROL_BASE;
+	localNumDigits = DTM1650_NUM_DIGITS;
+}
+
+void DTM1650::send_control(unsigned char data, uint8_t segment) {
+	segment = segment % DTM1650_NUM_DIGITS;
+	Wire.beginTransmission(localI2CControlAddress + segment);
+	Wire.write(data);
+	Wire.endTransmission();
+	DTM1650_CONTROL_STORE[segment] = data;
+}
+
+void DTM1650::send_control(unsigned char data) {
+	send_control(data, 0);
+}
+
+void DTM1650::send_digit(unsigned char data, uint8_t segment) {
+	segment = segment % DTM1650_NUM_DIGITS;
+	data = data & 0x7F;
+	data = data | (DTM1650_DIGITS_STORE[segment] & 0x80);
+	Wire.beginTransmission(localI2CDisplayAddress + segment);
+	Wire.write(data);
+	Wire.endTransmission();
+	DTM1650_DIGITS_STORE[segment] = data;
+}
+
+void DTM1650::send_digit(unsigned char data) {
+	send_digit(data, 0);
+}
+
+void DTM1650::set_dot(uint8_t segment, bool onoff) {
+	segment = segment % DTM1650_NUM_DIGITS;
+	unsigned char digit = DTM1650_DIGITS_STORE[segment];
+	digit = (onoff) ? digit | 0x80 : digit & 0x7f;
+	Wire.beginTransmission(localI2CDisplayAddress + segment);
+	Wire.write(digit);
+	Wire.endTransmission();
+	DTM1650_DIGITS_STORE[segment] = digit;
+}
+
+void DTM1650::set_brightness(unsigned char brightness, uint8_t segment) {
+	if (brightness > 0x07) {
+		brightness = 0x07;
+	}
+	segment = segment % DTM1650_NUM_DIGITS;
+	send_control(((DTM1650_CONTROL_STORE[segment] & DTM1650_MASK_BRIGHTNESS) | (brightness << 4)), segment);
+}
+
+void DTM1650::set_brightness(unsigned char brightness) {
+	set_brightness(brightness, 0);
+}
+
+void DTM1650::clear_display(void) {
+	for (uint8_t i = 0; i < localNumDigits; i++) {
+		send_digit(0x00, i);
+		DTM1650_DIGITS_STORE[i] = 0x00;
+	}
+}
+
+void DTM1650::display_on(void) {
+	for (uint8_t i = 0; i < localNumDigits; i++) {
+		send_control(((DTM1650_CONTROL_STORE[i] & DTM1650_MASK_ONOFF) | DTM1650_BIT_ONOFF), i);
+	}
+}
+
+void DTM1650::display_off(void) {
+	for (uint8_t i = 0; i < localNumDigits; i++) {
+		send_control((DTM1650_CONTROL_STORE[i] & DTM1650_MASK_ONOFF), i);
+	}
+}
+
+void DTM1650::colon_on(void) {
+	send_control((DTM1650_CONTROL_STORE[0] & DTM1650_MASK_COLON) | DTM1650_BIT_COLON);
+}
+
+void DTM1650::colon_off(void) {
+	send_control(DTM1650_CONTROL_STORE[0] & DTM1650_MASK_COLON);
+}
+
+void DTM1650::write_num(uint16_t num, uint8_t position) {
+	position = position % DTM1650_NUM_DIGITS;
+	num = num & 0x0F;
+	send_digit(DTM1650_Digit_Table[num], position);
+}
+
+void DTM1650::write_num(uint16_t num) {
+	uint8_t digit;
+	digit = num % 10;
+	send_digit(DTM1650_Digit_Table[digit], 3);
+	num = num / 10;
+	digit = num % 10;
+	send_digit(DTM1650_Digit_Table[digit], 2);
+	num = num / 10;
+	digit = num % 10;
+	send_digit(DTM1650_Digit_Table[digit], 1);
+	num = num / 10;
+	digit = num % 10;
+	send_digit(DTM1650_Digit_Table[digit], 0);
+}
+
+void DTM1650::write_time(uint16_t minute, uint16_t second) {
+	uint8_t digit;
+	digit = second % 10;
+	send_digit(DTM1650_Digit_Table[digit], 3);
+	second = second / 10;
+	digit = second % 10;
+	send_digit(DTM1650_Digit_Table[digit], 2);
+	set_dot(1, true);
+	digit = minute % 10;
+	send_digit(DTM1650_Digit_Table[digit], 1);
+	minute = minute / 10;
+	digit = minute % 10;
+	send_digit(DTM1650_Digit_Table[digit], 0);
+}
+
+void DTM1650::init(void) {
+	for (uint8_t i = 0; i < localNumDigits; i++) {
+		DTM1650_DIGITS_STORE[i] = 0x00;
+		DTM1650_CONTROL_STORE[i] = 0x00;
+	}
+	clear_display();
+	display_on();
+}
---
+++ libraries/DTM1650/src/DTM1650.h
@@ -0,0 +1,98 @@
+#ifndef DTM1650_H
+#define DTM1650_H
+
+#define DTM1650_VERSION			1.0
+#define DTM1650_CONTROL_BASE		0x24	// Address of the control register of the left-most digit
+#define DTM1650_DISPLAY_BASE		0x34	// Address of the left-most digit 
+#define DTM1650_NUM_DIGITS		4	// 4 segments
+
+#define DTM1650_MASK_BRIGHTNESS		0b10001111
+#define DTM1650_MASK_COLON			0b11110111
+#define DTM1650_BIT_COLON			0b00001000
+#define DTM1650_MASK_ONOFF			0b11111110
+#define DTM1650_BIT_ONOFF			0b00000001
+
+#define DTM1650_BRIGHTNESS_MAX		8
+#define DTM1650_BRIGHTNESS_MIN		1
+/*
+Brightness settings
+MSB				LSB
+B7 B6 B5 B4 B3 B2 B1 B0 Explanation
+ ×  0  0  0     ×  ×	8 brightness
+ ×  0  0  1     ×  ×	1 brightness
+ ×  0  1  0     ×  ×	2 brightness
+ ×  0  1  1     ×  ×	3 Brightness
+ ×  1  0  0     ×  ×	4 brightness
+ ×  1  0  1     ×  ×	5 brightness
+ ×  1  1  0     ×  ×	6 brightness
+ ×  1  1  1     ×  ×	7 brightness
+On / off the display position
+MSB				LSB
+B7 B6 B5 B4 B3 B2 B1 B0 Explanation
+ ×              ×  ×  0 Off Display
+ ×              ×  ×  1 On Display
+ */
+
+ /*
+	 0x01
+ 0x20		0x02
+	 0x40
+ 0x10		0x04
+	 0x08
+			 0x80
+  */
+
+const char DTM1650_Digit_Table[] = {
+	0x3F,	// 0
+	0x06,	// 1
+	0x5B,	// 2
+	0x4F,	// 3
+	0x66,	// 4
+	0x6D,	// 5
+	0x7D,	// 6
+	0x07,	// 7
+	0x7F,	// 8
+	0x6F,	// 9
+
+	0x77,
+	0x7C,
+	0x39,
+	0x5E,
+	0x79,
+	0x71
+};
+
+class DTM1650 {
+
+private:
+
+	uint8_t localI2CDisplayAddress;
+	uint8_t localI2CControlAddress;
+	uint8_t localNumDigits = DTM1650_NUM_DIGITS;
+	unsigned char DTM1650_DIGITS_STORE[DTM1650_NUM_DIGITS];
+	unsigned char DTM1650_CONTROL_STORE[DTM1650_NUM_DIGITS];
+
+public:
+
+	DTM1650(void);
+
+	void init(void);
+	void send_control(unsigned char Command);
+	void send_control(unsigned char Command, uint8_t Segment);
+	void send_digit(unsigned char Data);
+	void send_digit(unsigned char Data, uint8_t Segment);
+	void set_dot(uint8_t Segment, bool onoff);
+	void set_brightness(unsigned char Brightness);
+	void set_brightness(unsigned char Brightness, uint8_t Segment);
+	void display_on(void);
+	void display_off(void);
+	void colon_on(void);
+	void colon_off(void);
+	void clear_display(void);
+	void write_num(uint16_t num);
+	void write_num(uint16_t num, uint8_t position);
+	void write_time(uint16_t minute, uint16_t second);
+
+};
+
+#endif
